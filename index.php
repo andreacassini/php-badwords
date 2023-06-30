5b4637ffe9fde18a403815b16f60b01ef9608ab3
andreacassini <andrea1316@hotmail.it>
2023-06-30 14:35:46 +0200

    first commit, working form and returning data

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bad Words</title>
+    </head>
+    <body>
+        <form action="../run.php" method="GET">
+            <input type="text" name="testo">
+            <input type="text" name="parola_da_censurare">
+            <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ run.php
@@ -0,0 +1,6 @@
+<?php
+$testo = $_GET['testo'];
+$parola_da_censurare = $_GET['parola_da_censurare'];
+
+echo $testo." ".$parola_da_censurare;
+?>
