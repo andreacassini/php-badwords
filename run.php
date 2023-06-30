0f852133e04ee35c994ba304540b551b43835bf7
andreacassini <andrea1316@hotmail.it>
2023-06-30 14:56:50 +0200

    second step done

--- run.php
+++ run.php
@@ -2,6 +2,7 @@
 $testo = $_POST['testo'];
 $parola_da_censurare = $_POST['parola_da_censurare'];
 $lunghezza_testo = strlen($testo);
+$testo_censurato = str_replace($parola_da_censurare, "***", $testo);
 
-echo "<h2>Primo step</h2><br>Testo: ".$testo."<br> Numero caratteri: ".$lunghezza_testo."<br><h2>Secondo step</h2><br>Testo: ".$testo."<br> Numero caratteri: ".$lunghezza_testo;
+echo "<h2>Primo step</h2><br>Testo: ".$testo."<br> Numero caratteri: ".$lunghezza_testo."<br><h2>Secondo step</h2><br>Testo: ".$testo_censurato."<br> Numero caratteri: ".$lunghezza_testo;
 ?>
